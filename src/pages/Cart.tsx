43037d474bb32c685d68d9db2dc3ead810a1fdc9
Miguel G <sshushuy@gmail.com>
2023-11-03 17:56:10 -0600

    cart basic func

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,34 +1,57 @@
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material';
 import { useCartContext } from '../context/CartContext';
+import './Cart.css'
+import { Link } from 'react-router-dom';
 
 const CartView = () => {
   const { cartItems, removeFromCart, clearCart } = useCartContext();
-  console.log("🚀 ~ file: Cart.tsx:5 ~ CartView ~ cartItems:", {cartItems})
-
   return (
-    <div>
-      <h2>Cart</h2>
-      {cartItems.length === 0 ? (
-        <p>Your cart is empty.</p>
-      ) : (
-        <ul>
-          {cartItems.map((item) => (
-            <li key={item.id}>
-              <div>
-                <span>{item.title}</span>
-                <span>{item.price}</span>
-                <button onClick={() => removeFromCart(item.id)}>Remove</button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      )}
-
-      {cartItems.length > 0 && (
+    <div className='cart-container'>
+      <div>
         <div>
-          <p>Total: {cartItems.reduce((total, item) => total + item.price * item.quantity, 0)}</p>
-          <button onClick={clearCart}>Clear Cart</button>
+          <Typography variant='h4'>Cart</Typography>
+          {cartItems.length === 0 ? (
+            <p>Your cart is empty.</p>
+          ) : (
+            <div>
+              <ul>
+                {cartItems.map((item) => (
+                    <Card sx={{ maxWidth: 345,  margin: 2  }} className='cart-item'>
+                      <Link to={`/product/${item.id}`}>
+                        <CardMedia
+                          sx={{ height: 140, width: '140', display: 'flex', justifyContent: 'center', alignItems: 'center', alignContent: 'center' }}
+                          component={'img'}
+                          src={item.url}
+                        />
+                      </Link>
+                      <div>
+                        <CardContent>
+                          <Typography gutterBottom variant="h5" component="div">
+                            {item.title}
+                          </Typography>
+
+                        </CardContent>
+                        <CardActions className='cart-item-text'>
+                              <Button size="small">unidades: {item.quantity}</Button>
+                              <Button size="small">${item.price * item.quantity}</Button>
+                              <Button onClick={()=>removeFromCart(item.id)}>remover</Button>
+                        </CardActions>
+                      </div>
+                    </Card>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
-      )}
+
+        {cartItems.length > 0 && (
+          <div className='cart-total-text'>
+            <Typography gutterBottom variant="h5" component="div">Total: ${cartItems.reduce((total, item) => total + item.price * item.quantity, 0)}</Typography>
+            <button onClick={clearCart}>Clear Cart</button>
+          </div>
+        )}
+
+      </div>
     </div>
   );
 };
